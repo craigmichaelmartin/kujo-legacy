d32029c95becf8aa03148615ca55222a801ee90d
Craig Martin <craigmichaelmartin@gmail.com>
2016-09-06 21:00:10 -0400

    remove error handling causing errors

--- app.js
+++ app.js
@@ -34,36 +34,4 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-// Configure error handling
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error.html', {
-    message: err.message,
-    error: {}
-  });
-});
-
 module.exports = app;
